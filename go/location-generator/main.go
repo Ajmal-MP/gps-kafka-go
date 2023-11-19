5612dce5833b257db4ec2df7d631f8bae7c28dda
Ajmal <muhammed.ajmal@itspe.co.in>
2023-11-19 20:09:50 +0530

    send lat,long with vehicleID to kafka

--- go/location-generator/main.go
+++ go/location-generator/main.go
@@ -1,82 +1,60 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"math/rand"
-	"net/http"
-	"sync"
-	"time"
+
+	"github.com/IBM/sarama"
 )
 
-// Location struct to represent location data
 type Location struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
-}
-
-func generateRandomLocation() Location {
-	// For simplicity, generating random coordinates within a specific range.
-	// You might need to adjust this based on your requirements.
-	return Location{
-		Latitude:  rand.Float64()*180 - 90,
-		Longitude: rand.Float64()*360 - 180,
-	}
-}
-
-func sendLocationWorker(ch <-chan Location, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	url := "http://localhost:8081/location" // Update with your server URL
-
-	for loc := range ch {
-		jsonData, err := json.Marshal(loc)
-		if err != nil {
-			fmt.Println("Error encoding JSON:", err)
-			return
-		}
-
-		resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-		if err != nil {
-			fmt.Println("Error sending request:", err)
-			return
-		}
-		defer resp.Body.Close()
-
-		fmt.Println("Response Status:", resp.Status)
-	}
+	VehicleID int     `json:"vehicle_id"`
 }
 
 func main() {
-	// Seed the random number generator to get different values each time
-	rand.Seed(time.Now().UnixNano())
-
-	// Number of concurrent requests
-	numRequests := 10000
-	numWorkers := 6 // Number of workers to process requests simultaneously
-
-	// Create a buffered channel
-	requests := make(chan Location, numRequests)
-
-	// WaitGroup to wait for all workers to finish
-	var wg sync.WaitGroup
-
-	// Start workers
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go sendLocationWorker(requests, &wg)
+	currentLocation := Location{
+		Latitude:  37.7749,
+		Longitude: -122.4194,
+		VehicleID: 1,
 	}
 
-	// Enqueue location data
-	for i := 0; i < numRequests; i++ {
-		loc := generateRandomLocation()
-		requests <- loc
+	// Encode the Location struct to JSON
+	locationJSON, err := json.Marshal(currentLocation)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
 	}
 
-	// Close the channel to signal workers to finish
-	close(requests)
+	// Set up the Kafka producer configuration
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
 
-	// Wait for all workers to finish
-	wg.Wait()
+	brokerAddress := "139.5.189.204:9092" // Replace with your Kafka broker address
+	topic := fmt.Sprintf("vehicleID_%d", currentLocation.VehicleID)
+
+	// Create a Kafka producer
+	producer, err := sarama.NewSyncProducer([]string{brokerAddress}, config)
+	if err != nil {
+		fmt.Println("Error creating Kafka producer:", err)
+		return
+	}
+	defer producer.Close()
+
+	// Produce a message to the Kafka topic
+	message := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(locationJSON),
+	}
+
+	partition, offset, err := producer.SendMessage(message)
+	if err != nil {
+		fmt.Println("Error writing to Kafka:", err)
+		return
+	}
+
+	fmt.Printf("Message sent to partition %d at offset %d\n", partition, offset)
 }
